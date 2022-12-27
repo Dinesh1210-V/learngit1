77b49497bf69ddcb340ae3ee15f14ee26b1bb07e
Dinesh V <dineshvp21@gmail.com>
2022-12-27 16:58:57 +0530

    first time commite

---
+++ src/package_learngit1/firstclass.java
@@ -0,0 +1,10 @@
+package package_learngit1;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I start learning about git and github");
+	}
+
+}
